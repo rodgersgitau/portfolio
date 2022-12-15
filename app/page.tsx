9808f9fdaacd82492c8f6a97f870be714dd87f29
Rodgers M Gitau <rodgers.gitau@gmail.com>
2022-12-15 23:30:38 +0300

    feat: init project with nextjs13 & tailwind

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <section className="h-[100vh] w-full flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-semibold"> Hi, I'm Rodgers M Gitau</h1>
+    </section>
+  );
+};
+
+export default HomePage;
