8a0d677d08f429cca5558d741b198c0f01fd810d
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-01-02 23:30:20 +0300

    feat: add portfolio placeholders

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="m-0 p-0 box-content">
-        <section className="min-h-screen h-max p-8 bg-[#1d1d20] text-gray-100 relative">
-          <div className="h-full relative container mx-auto flex flex-col gap-10 px-8">
+      <body className="m-0 p-0 overflow-x-hidden overflow-y-auto">
+        <section className="h-full min-h-screen w-screen p-8 bg-[#1d1d20] text-gray-100 relative flex flex-col gap-8">
+          <div className="relative container mx-auto flex flex-col gap-8 px-2 md:px-8">
             <div className="absolute top-0 left-0 z-10 ">
               <NextImage
                 alt=""
@@ -34,7 +34,7 @@ export default function RootLayout({
               />
             </div>
             <Navigation />
-            <div className="min-h-[70vh]">{children}</div>
+            <div className="h-full w-full min-h-[68vh]">{children}</div>
           </div>
           <footer className="container mx-auto flex flex-col md:flex-row md:items-center gap-20">
             <p className="text-slate-200 ">&copy; Copyright 2022</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "fit-25": "repeat(auto-fit, minmax(25rem, 1fr))",
+        "fill-25": "repeat(auto-fill, minmax(25rem, 1fr))",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
