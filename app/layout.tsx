edd1362ec2880578306be3d55d89e9a77b038e32
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-01-16 21:22:33 +0300

    fix: styling & background
    - add image to landing page bg
    - fix navigation items
    - add card component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import NextImage from "next/image";
 
 import "../styles/globals.css";
 import { Navigation } from "../components";
+import { FaEnvelopeSquare, FaPhone, FaPhoneSquareAlt } from "react-icons/fa";
 
 export default function RootLayout({
   children,
@@ -12,8 +13,8 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="m-0 p-0 overflow-x-hidden overflow-y-auto">
-        <section className="h-full min-h-screen w-screen p-8 bg-[#1d1d20] text-gray-100 relative flex flex-col gap-8">
+      <body className="m-0 p-0 overflow-x-hidden overflow-y-auto bg-[#1d1d20] text-gray-100">
+        <section className="h-full min-h-screen w-screen p-8 bg-ascii-pattern bg-no-repeat bg-right-bottom  relative flex flex-col gap-8">
           <div className="relative container mx-auto flex flex-col gap-8 px-2 md:px-8">
             <div className="absolute top-0 left-0 z-10 ">
               <NextImage
@@ -36,10 +37,16 @@ export default function RootLayout({
             <Navigation />
             <div className="h-full w-full min-h-[68vh]">{children}</div>
           </div>
-          <footer className="container mx-auto flex flex-col md:flex-row md:items-center gap-20">
-            <p className="text-slate-200 ">&copy; Copyright 2022</p>
-            <p className="text-slate-200 ">Tel: +254 711408297</p>
-            <p className="text-slate-200 ">Email: rodgersgitau@gmail.com</p>
+          <footer className="container mx-auto flex flex-col md:flex-row md:items-center gap-20 text-slate-200">
+            <p>&copy; Copyright 2022</p>
+            <div className="flex items-center gap-1">
+              <FaPhoneSquareAlt fontSize="1.25rem" />
+              <span>+254 759085920</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <FaEnvelopeSquare fontSize="1.25rem" />
+              <span>rodgersgitau@gmail.com</span>
+            </div>
           </footer>
         </section>
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "ascii-pattern": "url('/images/ascii-pattern.svg')",
+      },
       gridTemplateColumns: {
         "fit-25": "repeat(auto-fit, minmax(25rem, 1fr))",
         "fill-25": "repeat(auto-fill, minmax(25rem, 1fr))",
