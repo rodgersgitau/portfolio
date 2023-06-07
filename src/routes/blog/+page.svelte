65a8af55599444a12de1703f21220847d0b8df2b
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-06-07 23:59:07 +0300

    feat: config mdsvex

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+</script>
+
+<div class="min-h-[65vh] h-max overflow-y-auto">
+	<h1 class="font-sans text-4xl font-bold leading-loose">Stories and Exploration</h1>
+
+	<ul class="links">
+		{#each data.posts as post}
+			<li>
+				<a href={`blog/${post.slug}`}>{post.title}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
