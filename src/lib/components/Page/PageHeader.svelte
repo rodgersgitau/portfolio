9f59b19618786d2eb679ae0b2dd2dac11dc49ee0
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-06-09 01:27:38 +0300

    feat: blog content

--- src/lib/components/Page/PageHeader.svelte
+++ src/lib/components/Page/PageHeader.svelte
@@ -7,14 +7,16 @@
 	class="w-full min-h-[10vh] shadow border rounded-md border-zinc-300 p-8 bg-zinc-100/30 dark:border-zinc-600 dark:bg-black/30"
 >
 	<div class="flex flex-col items-center gap-10 xl:flex-row">
-		<div class="flex flex-col justify-center flex-grow gap-6">
-			<h1 class="text-3xl xl:text-4xl">{pageTitle}</h1>
-			<h4 class="font-normal text-zinc-600 dark:text-zinc-400">{pageDescription}</h4>
+		<div class="flex flex-col justify-center gap-6 xl:w-2/3">
+			<h1 class="text-xl font-medium leading-4 capitalize lg:text-2xl">
+				{pageTitle}
+			</h1>
+			<h4 class="text-lg font-medium text-zinc-600 dark:text-zinc-400">{pageDescription}</h4>
 		</div>
 
 		<!-- Subscription form -->
-		<form on:submit|preventDefault class="w-full xl:w-max">
-			<div class="flex flex-col gap-4 xl:flex-row xl:items-center">
+		<form on:submit|preventDefault class="w-full xl:w-1/3">
+			<div class="flex flex-col gap-4">
 				<input
 					id="email"
 					type="email"
