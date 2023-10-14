48f3f94cc298be36a4e8ae338b2848489437c680
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-10-14 23:49:14 +0300

    style: animations & effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,114 +10,122 @@ import { FiCodesandbox } from "react-icons/fi";
 import { Image, IWorkItem } from "../components";
 import Section from "../components/Section";
 import AnimatedWords from "../motion/components/AnimatedWords";
-import { dropWithFade, staggerChildren } from "../motion/animations";
+import {
+  dropWithFade,
+  riseWithFade,
+  staggerChildren,
+} from "../motion/animations";
 
 const HomePage = ({ projects }: { projects: IWorkItem[] }) => {
   return (
     <motion.div
-      initial="initial"
-      animate="animate"
+      variants={riseWithFade}
       className="h-full min-h-[60vh] w-full flex flex-col gap-16 items-center justify-center"
     >
-      <div className="flex flex-col-reverse items-center w-full gap-40 py-4 lg:flex-row">
-        <div className="w-full lg:w-[50%] h-full flex flex-col gap-8 ">
-          <h1 className="flex items-center gap-4 font-semibold">
-            <span className="text-3xl">Hello, I'm</span>
-            <span className="relative inline-block p-1 px-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-black before:dark:bg-pink-500 before:rounded-md">
-              <span className="relative mx-auto text-2xl text-white">
-                Rodgers M Gitau
+      <Section className="w-full my-8">
+        <motion.div
+          variants={riseWithFade}
+          className="flex flex-col-reverse items-center w-full gap-40 py-4 lg:flex-row"
+        >
+          <div className="w-full lg:w-[50%] h-full flex flex-col gap-8 ">
+            <h1 className="flex items-center gap-4 font-semibold">
+              <span className="text-3xl">Hello, I'm</span>
+              <span className="relative inline-block align-middle p-[4px] before:h-[105%] before:absolute before:-inset-1 before:top-[50%] before:-skew-y-3 before:-translate-y-[50%] before:bg-black before:dark:bg-pink-500 before:rounded-md">
+                <span className="relative mx-auto text-2xl text-white">
+                  <AnimatedWords title="Rodgers M Gitau" />
+                </span>
               </span>
-            </span>
-          </h1>
-          <div className="flex flex-col gap-8 text-lg text-slate-800 dark:text-slate-400">
-            <p>
-              I'm a full-stack software engineer based in Nairobi, Kenya. For
-              the past 7+ years, I've designed, developed & shipped all sorts of
-              web applications for a myriad of industries i.e hospitality, real
-              estate, ecommerce & healthcare
-            </p>
-            <p>
-              I'm fascinated with cutting-edge, pixel-perfect, beautiful
-              interfaces and intuitively implemented UX.
-            </p>
+            </h1>
+            <div className="flex flex-col gap-8 text-lg text-slate-800 dark:text-slate-400">
+              <p>
+                I'm a full-stack software engineer based in Nairobi, Kenya. For
+                the past 7+ years, I've designed, developed & shipped all sorts
+                of web applications for a myriad of industries i.e hospitality,
+                real estate, ecommerce & healthcare
+              </p>
+              <p>
+                I'm fascinated with cutting-edge, pixel-perfect, beautiful
+                interfaces and intuitively implemented UX.
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="w-full md:w-[50vw] flex items-center justify-center gap-8">
-          <div className="relative z-10 transition duration-300 -inset-1 -skew-y-2">
-            <Image
-              shadow
-              width={380}
-              height={760}
-              alt="a photo of me"
-              src="/images/me.jpg"
-              className="rounded-lg"
-            />
+          <div className="w-full md:w-[50vw] flex items-center justify-center gap-8">
+            <div className="relative z-10 transition duration-300 -inset-1 -skew-y-2">
+              <Image
+                shadow
+                width={380}
+                height={760}
+                alt="a photo of me"
+                src="/images/me.jpg"
+                className="rounded-lg"
+              />
+            </div>
           </div>
+        </motion.div>
+        <div className="flex items-center w-full h-full gap-12">
+          <a
+            title="Github"
+            aria-label="github profile"
+            referrerPolicy="no-referrer"
+            href="https://github.com/rodgersgitau"
+            className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
+          >
+            <FaGithubSquare className="w-12 h-12 rounded-md" />
+            <p className="flex-1 text-sm">Github</p>
+          </a>
+          <a
+            title="LinkedIn"
+            aria-label="linked in profile"
+            href="https://www.linkedin.com/in/rodgersgitau"
+            className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
+          >
+            <FaLinkedin className="w-12 h-12 rounded-md" />
+            <p className="flex-1 text-sm">LinkedIn</p>
+          </a>
+          <a
+            title="Code Sandbox"
+            aria-label="code sandbox profile"
+            href="https://codesandbox.io/u/rodgersgitau"
+            className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
+          >
+            <FiCodesandbox className="w-12 h-12 rounded-md" />
+            <p className="flex-1 text-sm">CodeSandbox</p>
+          </a>
+          <a
+            download={true}
+            title="My Resume"
+            aria-label="download my resume"
+            href="/documents/Rodgers M Gitau CV.pdf"
+            className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
+          >
+            <FaFilePdf className="w-12 h-12 rounded-md" />
+            <p className="flex-1 text-sm">Resume</p>
+          </a>
         </div>
-      </div>
-      <div className="flex items-center w-full h-full gap-12">
-        <a
-          title="Github"
-          aria-label="github profile"
-          referrerPolicy="no-referrer"
-          href="https://github.com/rodgersgitau"
-          className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
-        >
-          <FaGithubSquare className="w-12 h-12 rounded-md" />
-          <p className="flex-1 text-sm">Github</p>
-        </a>
-        <a
-          title="LinkedIn"
-          aria-label="linked in profile"
-          href="https://www.linkedin.com/in/rodgersgitau"
-          className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
-        >
-          <FaLinkedin className="w-12 h-12 rounded-md" />
-          <p className="flex-1 text-sm">LinkedIn</p>
-        </a>
-        <a
-          title="Code Sandbox"
-          aria-label="code sandbox profile"
-          href="https://codesandbox.io/u/rodgersgitau"
-          className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
-        >
-          <FiCodesandbox className="w-12 h-12 rounded-md" />
-          <p className="flex-1 text-sm">CodeSandbox</p>
-        </a>
-        <a
-          download={true}
-          title="My Resume"
-          aria-label="download my resume"
-          href="/documents/Rodgers M Gitau CV.pdf"
-          className="flex flex-col items-center justify-center gap-4 text-center transition-all ease-out hover:text-pink-600 hover:-translate-y-2"
-        >
-          <FaFilePdf className="w-12 h-12 rounded-md" />
-          <p className="flex-1 text-sm">Resume</p>
-        </a>
-      </div>
-      <Section title="projects" className="w-full my-8 lg:mx-auto lg:max-w-7xl">
+      </Section>
+      <Section title="projects" className="w-full mx-auto my-8">
         <motion.div
           variants={staggerChildren}
-          className="grid w-full h-full grid-cols-1 gap-8 my-10 lg:gap-20 lg:grid-cols-2"
+          className="grid w-full h-full grid-cols-1 gap-20 mx-auto my-10 max-w-7xl lg:grid-cols-2"
         >
           {projects.map((project, idx) => (
             <motion.div
               key={project.slug}
               variants={dropWithFade}
-              className="relative w-full group lg:min-h-[45rem] ease-in-out"
+              className={`relative w-full h-full group ease-in-out ${
+                idx % 2 === 1
+                  ? "top-[15%] -translate-y-[15%]"
+                  : "top-0 translate-y-0"
+              }`}
             >
               <div
-                className={`absolute transition duration-1000 rounded-lg opacity-20 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur group-hover:opacity-100 group-hover:duration-200 ${
-                  idx % 2 === 1 ? "translate-y-[15%]" : "translate-y-0"
-                }`}
+                className={`absolute transition duration-1000 rounded-lg opacity-20 -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 blur group-hover:opacity-100 group-hover:duration-200`}
               />
               <div
-                className={`relative flex flex-col justify-center w-full h-full gap-4 p-8 rounded-lg lg:gap-20 bg-slate-100 text-black/70 dark:text-slate-100 dark:bg-black/90 group-hover:dark:bg-black group-hover:bg-slate-100 ring-slate-400/5 ring-1 dark:ring-slate-900/5 ${
-                  idx % 2 === 1 ? "translate-y-[15%]" : "translate-y-0"
-                }`}
+                className={`relative flex flex-col justify-center min-h-[35rem] gap-8 p-8 rounded-lg bg-slate-100 text-black/70 dark:text-slate-100 dark:bg-black/90 group-hover:dark:bg-black group-hover:bg-slate-100 ring-slate-400/5 ring-1 dark:ring-slate-900/5`}
               >
-                <div className="relative flex-1 overflow-hidden rounded-t-lg shadow lg:h-2/3 min-h-[15rem] shadow-current/90 lg:rounded-lg">
+                <div className="relative flex-1 overflow-hidden rounded-t-lg shadow aspect-video min-h-[15rem] shadow-current/90 lg:rounded-lg">
                   <NextImage
                     alt=""
                     fill
